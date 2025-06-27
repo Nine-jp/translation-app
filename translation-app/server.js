8375973a2e89f1eaea72be073ac5ce9d3dd0f3da
Nine-jp <nine.jp.4nft@gmail.com>
2025-06-27 10:04:43 +0900

    fix: Bind to Heroku's dynamic PORT

--- translation-app/server.js
+++ translation-app/server.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // JSONリクエストボディをパースするためのミドルウェア
 app.use(express.json());
